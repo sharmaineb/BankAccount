755b26f4d5d90545c1812989654f4129e59fbcea
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-10-24 20:59:40 -0700

    added placeholders

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,19 @@
 class BankAccount:
-    def __init__(self, full_name, account_number, balance = 0):
+    def __init__(self, full_name, account_number, balance = 0): # balance starting at 0
         self.name = full_name
         self.account = account_number
         self.balance = balance
         
+    def deposit(self, amount):
+        self.balance = self.balance + amount # add amount to the balance
+        print("Amount Deposited: $$$ New Balance: $$$") # placeholder
+
+    def withdraw(self, amount, amount_to_withdraw):
+        self.balance = self.balance - amount # subtract balance from account
+        print("Amount Withdrawn: $$$ New Balance: $$$") # placeholder
+        if amount_to_withdraw > self.balance:
+            print("Insufficient Funds.")
+            # charge overdraft fee of $10
+
+    def get_balance():
+        print("Here is your current account balance:")
