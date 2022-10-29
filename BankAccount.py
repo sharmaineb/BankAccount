6aa8ba17180dc71dafaf693887d2bffab3545450
Sharmaine <sharmaine.borbe@students.dominican.edu>
2022-10-28 22:11:18 -0700

    worked through bugs

--- BankAccount.py
+++ BankAccount.py
@@ -1,5 +1,7 @@
+accounts_list = []
+
 class BankAccount:
-    def __init__(self, full_name, account_number, type_of_account, balance = 0,): # balance starting at 0
+    def __init__(self, full_name, account_number, type_of_account, balance = 0,): # balance set at 0
         self.name = full_name
         self.account = account_number
         self.balance = balance
@@ -24,7 +26,7 @@ class BankAccount:
         print("Here is your current account balance:")
         return self.balance
 
-    def add_interest(self):
+    def add_interest(self): # check the type of bank account: checking/savings
         if self.account_type == "checking":
             interest = self.balance * 0.00083 # add interest to the users balance
             self.balance = self.balance + interest
@@ -34,9 +36,13 @@ class BankAccount:
 
     def print_statement(self): # prints a message with the account name, account number, and balance
         print()
-        print(  "Name:", self.name)
+        print(  "Name:", self.name) # name
         print(  "Account No.:", self.account ) # account number
-        print(  "Balance:", self.balance)
+        print(  "Balance:", self.balance) # balance
+
+    def new_account(self):
+        new_account = {"Name:", self.name, "Balance:", self.balance, "Account Number:", self.account, "Type:", self.account_type}
+        accounts_list.append(new_account)
 
 # outside of the BankAccount class, define 3 different bank account examples using the BankAccount() object.
 shar = BankAccount("Shar", "67982312", "checking")
@@ -51,6 +57,7 @@ ian = BankAccount("Ian", "112163201", "savings")
 ian.deposit(10000)
 ian.print_statement()
 ian.withdraw(1000)
+ian.print_statement()
 ian.add_interest()
 ian.print_statement()
 
@@ -58,12 +65,23 @@ kae = BankAccount("Kaeleen", "671507134", "checking")
 kae.deposit(12000)
 kae.print_statement()
 kae.add_interest()
+kae.print_statement()
 kae.withdraw(100)
 kae.print_statement()
 
 # example code
-mitchell = BankAccount("Mitchell", "3141592")
+mitchell = BankAccount("Mitchell", "3141592", "savings")
 mitchell.deposit(400000)
 mitchell.print_statement()
 mitchell.withdraw(150)
 mitchell.print_statement()
+mitchell.add_interest()
+mitchell.print_statement()
+
+# Create a list called: bank. Add all of your accounts to bank. 
+bank = [shar, ian, kae, mitchell]
+
+
+
+
+
